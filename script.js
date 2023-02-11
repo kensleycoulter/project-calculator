74a1b8f1f9d30347bf2a1714be4088182bd28580
Kensley Coulter <kensleycoulter@gmail.com>
2023-02-11 14:04:23 -0600

    Update display & set operator

--- script.js
+++ script.js
@@ -29,23 +29,140 @@ console.log(divide(1,900)); //.001
 
 
 
-const operate = function (num1, num2, operator) { 
-   const addOP =  '+';
+const operate = function (a, b, operator) { 
+   /* const addOP =  '+';
    const subtractOP = '-';
    const multiplyOP = '*';
-   const divideOP = '/';
+   const divideOP = '/'; */
 
-    if(operator === addOP) {
-        return add(num1,num2);
-    } else if (operator === subtractOP){
-        return subtract(num1, num2);
-    } else if (operator === multiplyOP){
-        return multiply(num1, num2);
-    } else (operator === divideOP) 
-        return divide(num1, num2); 
+    if(operator === '+') {
+        return add(a, b);
+    } else if (operator === '-'){
+        return subtract(a, b);
+    } else if (operator === 'x'){
+        return multiply(a, b);
+    } else (operator === '/') 
+        return divide(a, b); 
 };
 
 console.log(operate(2,2,'+'));
 console.log(operate(2,2,'-'));
 console.log(operate(2,2,'*'));
 console.log(operate(2,2,'/'));
+
+let firstOperand;
+let secondOperand;
+let currentOperator;
+let shouldResetScreen = false; 
+
+let previousOperandDisplay = document.querySelector('.previous-operand');
+let currentOperandDisplay = document.querySelector('.current-operand');
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+
+
+operationButtons.forEach((btn) =>
+  btn.addEventListener('click', () => setOperation(btn.textContent))
+)
+
+
+const updateDisplay = function (numberButtons) {
+    //if (currentOperandDisplay.textContent === '0' || shouldResetScreen)
+    resetScreen()
+    currentOperandDisplay.textContent += numberButtons.value + ' ';
+    //console.log(firstOperand);
+} 
+
+
+const setOperation = function (operator) {
+
+    //evaluate();
+   firstOperand = currentOperandDisplay.textContent
+    //operator = '+';
+    currentOperator = operator;
+
+    previousOperandDisplay.textContent = `${firstOperand} ${currentOperator}`;
+    //console.log(firstOperand);
+   // console.log(currentOperator);
+
+}
+  
+const evaluate = function () {
+    secondOperand = currentOperandDisplay.textContent;
+    //console.log(secondOperand);
+    currentOperandDisplay.textContent = operate(firstOperand, secondOperand, '+');
+    previousOperandDisplay.textContent = `${firstOperand} ${currentOperator} ${secondOperand} =`
+    console.log(firstOperand);
+    console.log(secondOperand);
+    console.log(currentOperator);
+    console.log(operate(7,1,'+'));
+    console.log(operate(firstOperand,secondOperand,currentOperator));
+}
+
+const clear = function () {
+    currentOperandDisplay.textContent = 0;
+    previousOperandDisplay.textContent = '';
+    firstOperand = '';
+    secondOperand = '';
+    currentOperator = null; 
+}
+
+const resetScreen = function () {
+    currentOperandDisplay.textContent = '';
+    //shouldResetScreen = false;
+}
+
+/* operationButtons.forEach((btn => { 
+    btn.addEventListener = 'click', () => {
+        console.log('mirrornball') 
+    }
+    console.log(btn)
+}))
+ */
+
+//console.log(calculatorData);
+
+/* function checkData(){
+    previousOperandDisplay
+    .textContent = calculatorData;
+}
+
+ 
+checkData();
+ */
+//addDisplayValue();
+
+//document.querySelector('.btn').addEventListener('click', addDisplayValue);
+
+/* document.addEventListener('DOMContentLoaded', () => {
+    let buttons = document.querySelectorAll('.btn');
+    buttons.addEventListener('click', addDisplayValue); 
+})   */
+
+/* let buttons = document.querySelector('.btn');
+    buttons.addEventListener('click', populateDisplay);  */
+
+
+/*  const updateDisplayValue = () => {
+   // currentDisplay 
+    currentDisplay.textContent = displayValue; 
+}  */
+
+//updateDisplayValue();
+
+//console.log(addDisplayValue);
+
+//displayValue.populateDisplay(choice);
+
+//console.log(calculatorData);
+
+
+//operationButtons.addEventListener('click', operate);
+//operationButtons.addEventListener('click', console.log('pls work'));
+equalsButton.addEventListener('click', evaluate);
+allClearButton.addEventListener('click', clear);
+
