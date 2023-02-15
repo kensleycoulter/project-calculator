99b4a56ae6726bf853dee75f44f11cb97929271e
Kensley Coulter <kensleycoulter@gmail.com>
2023-02-15 13:50:28 -0600

    Debug to enable stringing of multiple operations
    i.e. 12+7-5*3 = 42

--- script.js
+++ script.js
@@ -1,4 +1,90 @@
 
+let firstOperand;
+let secondOperand;
+let currentOperator;
+let nextOperator = null;
+let shouldResetScreen = false; 
+
+let previousOperandDisplay = document.querySelector('.previous-operand');
+let currentOperandDisplay = document.querySelector('.current-operand');
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+
+
+operationButtons.forEach((btn) =>
+  btn.addEventListener('click', () => setOperation(btn.textContent))
+)
+
+const updateDisplay = function (numberButtons) {
+    if (currentOperandDisplay.textContent === '0' || shouldResetScreen) {
+        resetScreen()
+    }
+    currentOperandDisplay.textContent += numberButtons.value;
+    //console.log(firstOperand);
+} 
+
+
+const setOperation = function (operator) {
+    if(currentOperator != null) {
+        evaluate();
+    }
+
+   firstOperand = currentOperandDisplay.textContent
+    //operator = '+';
+    currentOperator = operator;
+
+    previousOperandDisplay.textContent = `${firstOperand} ${currentOperator}`;
+    shouldResetScreen = true
+    
+
+    //console.log(firstOperand);
+   // console.log(currentOperator);
+
+}
+  
+const evaluate = function () {
+    if(currentOperator === null || shouldResetScreen) return
+    if(currentOperator === '/' && currentOperandDisplay.textContent === '0'){
+    alert('you can\t divide by 0 loser')
+    return
+   }
+
+    secondOperand = currentOperandDisplay.textContent;
+
+
+    //console.log(secondOperand);
+    currentOperandDisplay.textContent = operate(firstOperand, secondOperand, currentOperator);
+    previousOperandDisplay.textContent = `${firstOperand} ${currentOperator} ${secondOperand} =`
+    console.log(firstOperand);
+    console.log(secondOperand);
+    console.log(currentOperator);
+
+    console.log(operate(7,0,'/'));
+    console.log(operate(7,0,'÷'));
+
+    console.log(operate(firstOperand,secondOperand,currentOperator));
+}
+
+
+
+const clear = function () {
+    currentOperandDisplay.textContent = 0;
+    previousOperandDisplay.textContent = '';
+    firstOperand = '';
+    secondOperand = '';
+    currentOperator = null; 
+}
+
+const resetScreen = function () {
+    currentOperandDisplay.textContent = '';
+    shouldResetScreen = false;
+}
+
+
 const add = function(a,b) {
 	sum = +a + +b;
     return Math.round(sum * 1000)/1000;
@@ -18,7 +104,7 @@ const multiply = function(a,b) {
 console.log(multiply(2.78459,1.74934)); //4.871
 
 const divide = function(a,b) {
-   if(b === 0) {
+   if(b === 0 ) {
     return 'you can\'t divide by 0!';
    } else {
     quotient = a / b;
@@ -30,10 +116,6 @@ console.log(divide(1,900)); //.001
 
 
 const operate = function (a, b, operator) { 
-   /* const addOP =  '+';
-   const subtractOP = '-';
-   const multiplyOP = '*';
-   const divideOP = '/'; */
 
     if(operator === '+') {
         return add(a, b);
@@ -41,83 +123,15 @@ const operate = function (a, b, operator) {
         return subtract(a, b);
     } else if (operator === 'x'){
         return multiply(a, b);
-    } else (operator === '/') 
+    } else (operator === '÷')
         return divide(a, b); 
 };
 
+console.log(operate(7,0,'÷'))
 console.log(operate(2,2,'+'));
 console.log(operate(2,2,'-'));
-console.log(operate(2,2,'*'));
-console.log(operate(2,2,'/'));
-
-let firstOperand;
-let secondOperand;
-let currentOperator;
-let shouldResetScreen = false; 
-
-let previousOperandDisplay = document.querySelector('.previous-operand');
-let currentOperandDisplay = document.querySelector('.current-operand');
-
-const numberButtons = document.querySelectorAll('[data-number]');
-const operationButtons = document.querySelectorAll('[data-operation]');
-const equalsButton = document.querySelector('[data-equals]');
-const deleteButton = document.querySelector('[data-delete]');
-const allClearButton = document.querySelector('[data-all-clear]');
-
-
-operationButtons.forEach((btn) =>
-  btn.addEventListener('click', () => setOperation(btn.textContent))
-)
-
-
-const updateDisplay = function (numberButtons) {
-    if (currentOperandDisplay.textContent === '0' || shouldResetScreen) {
-        resetScreen()
-    }
-    currentOperandDisplay.textContent += numberButtons.value;
-    //console.log(firstOperand);
-} 
-
-
-const setOperation = function (operator) {
-
-    //evaluate();
-   firstOperand = currentOperandDisplay.textContent
-    //operator = '+';
-    currentOperator = operator;
-
-    previousOperandDisplay.textContent = `${firstOperand} ${currentOperator}`;
-    shouldResetScreen = true
-
-    //console.log(firstOperand);
-   // console.log(currentOperator);
-
-}
-  
-const evaluate = function () {
-    secondOperand = currentOperandDisplay.textContent;
-    //console.log(secondOperand);
-    currentOperandDisplay.textContent = operate(firstOperand, secondOperand, currentOperator);
-    previousOperandDisplay.textContent = `${firstOperand} ${currentOperator} ${secondOperand} =`
-    console.log(firstOperand);
-    console.log(secondOperand);
-    console.log(currentOperator);
-    console.log(operate(7,1,'+'));
-    console.log(operate(firstOperand,secondOperand,currentOperator));
-}
-
-const clear = function () {
-    currentOperandDisplay.textContent = 0;
-    previousOperandDisplay.textContent = '';
-    firstOperand = '';
-    secondOperand = '';
-    currentOperator = null; 
-}
-
-const resetScreen = function () {
-    currentOperandDisplay.textContent = '';
-    shouldResetScreen = false;
-}
+console.log(operate(2,2,'x'));
+console.log(operate(2,2,'÷'));
 
 
 equalsButton.addEventListener('click', evaluate);
